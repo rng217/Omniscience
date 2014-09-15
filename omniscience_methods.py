820d4d6512f08663be881cfa67ebc39cd9563693
Daniel Wilson <DanielWilson.Champaign@gmail.com>
2014-09-15 01:05:27 -0500

    Updated Screenshot Analysis.
    Added support for 4:3 and 16:10 resolutions.
    Made recognition more robust, will now tell you if there are errors.

--- omniscience_methods.py
+++ omniscience_methods.py
@@ -1,5 +1,9 @@
 from omniscience_tables import *
+import tkMessageBox
 import difflib
+import Image
+import ImageGrab
+import ImageStat
 
 # Odds can be multiplied together easily
 def to_odds(x):
@@ -36,39 +40,49 @@ def get_next_in_order(order,grid_frame):
             if first_empty==None: first_empty=entry
     return (new_order,first_empty)
 
-# Analyze a Captains Draft screenshot
-def screenshot_CD():
-    import Image
-    import ImageGrab
-    import ImageStat
+# Analyze a Random Draft screenshot
+def screenshot_RD():
     pool=[]
     screenshot=ImageGrab.grab()
-    rescale=float(screenshot.size[0])/1920
+    rescale=float(screenshot.size[1])/1080
+    aspect = 16*screenshot.size[1]/screenshot.size[0]
+    if aspect not in hero_group_pixel_offsets_RD:
+        tkMessageBox.showerror("Error","Aspect Ratio not supported")
+        return
+    for group in range(6):
+        for hero_number_within_group in range(len(hero_group_table[group])):
+            blockX,blockY=hero_group_pixel_offsets_RD[aspect][group]
+            offset = 64 if aspect==12 else 73 # The 4:3 icons are smaller
+            image=screenshot.crop((int((blockX+offset*(hero_number_within_group%7)+2)*rescale), int((blockY+offset*(hero_number_within_group/7))*rescale),\
+                                                      int((blockX+offset*(hero_number_within_group%7)+51)*rescale), int((blockY+offset*(hero_number_within_group/7)+51)*rescale)))
+            #image.show()
+            brightness=max(ImageStat.Stat(image).mean)
+            if heroes[hero_group_table[group][hero_number_within_group]]=="Spectre":
+                print brightness
+                brightness+=23 #spectre is super dark
+            if brightness>46.6:
+                pool.append(hero_group_table[group][hero_number_within_group])
+    if len(pool)>24 or len(pool)<14: tkMessageBox.showwarning("Warning","Warning! Number of heroes detected ("+str(len(pool))+") is wrong!")
+    return pool
+
+# Analyze a Captains Draft screenshot
+def screenshot_CD():
+    pool=[]
+    screenshot=ImageGrab.grab()
+    rescale=float(screenshot.size[1])/1080
+    aspect = 16*screenshot.size[1]/screenshot.size[0]
+    if aspect not in hero_group_pixel_offsets_RD:
+        tkMessageBox.showerror("Error","Aspect Ratio not supported")
+        return
     for group in range(6):
         for hero_number_within_group in range(len(hero_group_table[group])):
             if hero_group_table[group][hero_number_within_group] not in cmhero_range: continue #phoenix is a black box and is messing up detection
-            blockX,blockY=[(292,267),(630,267),(292,526),(630,526),(292,786),(630,786)][group]
-            stats=ImageStat.Stat(screenshot.crop((int((blockX+81*(hero_number_within_group%4)+2)*rescale), int((blockY+46*(hero_number_within_group/4)+2)*rescale),\
-                                                      int((blockX+81*(hero_number_within_group%4)+78-2)*rescale), int((blockY+46*(hero_number_within_group/4)+44-2)*rescale))))
+            blockX,blockY=hero_group_pixel_offsets_CD[aspect][group]
+            image = screenshot.crop((int((blockX+81*(hero_number_within_group%4)+2)*rescale), int((blockY+46*(hero_number_within_group/4)+2)*rescale),\
+                                     int((blockX+81*(hero_number_within_group%4)+78-2)*rescale), int((blockY+46*(hero_number_within_group/4)+44-2)*rescale)))
+            #image.show()
+            stats=ImageStat.Stat(image)
             if max(stats.mean)>20.5: #puck is brightest at 20.15
                 pool.append(hero_group_table[group][hero_number_within_group])
-    return pool
-
-# Analyze a Random Draft screenshot
-def screenshot_RD():
-    import Image
-    import ImageGrab
-    import ImageStat
-    pool=[]
-    screenshot=ImageGrab.grab()
-    rescale=float(screenshot.size[0])/1920
-    for group in range(6):
-        for hero_number_within_group in range(len(hero_group_table[group])):
-            blockX,blockY=[(183,235),(183,469),(710,235),(710,469),(1242,235),(1242,469)][group]
-            image=screenshot.crop((int((blockX+73*(hero_number_within_group%7)+2)*rescale), int((blockY+73*(hero_number_within_group/7))*rescale),\
-                                                      int((blockX+73*(hero_number_within_group%7)+59)*rescale), int((blockY+73*(hero_number_within_group/7)+59)*rescale)))
-            brightness=max(ImageStat.Stat(image).mean)
-            if heroes[hero_group_table[group][hero_number_within_group]]=="Spectre": brightness+=15 #spectre is super dark
-            if brightness>46.6:
-                pool.append(hero_group_table[group][hero_number_within_group])
+    if len(pool)>27 or len(pool)<11: tkMessageBox.showwarning("Warning","Warning! Number of heroes detected ("+str(len(pool))+") is wrong!")
     return pool
